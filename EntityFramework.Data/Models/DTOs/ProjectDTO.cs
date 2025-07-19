d9d5dd2700566104da56afd3c4b983b74aa726d5
karim.albaghdady <karim.albaghdady@nt-me.com>
2025-07-19 15:06:25 +0300

    add Model Validation

--- EntityFramework.Data/Models/DTOs/ProjectDTO.cs
+++ EntityFramework.Data/Models/DTOs/ProjectDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace EntityFramework.Data.Models.DTOs
 {
     public class ProjectDTO
     {
+        [Required]
+        [MaxLength(100,ErrorMessage ="Max length should be 100")]
         public string Name { get; set; }
         public string Description { get; set; }
     }
